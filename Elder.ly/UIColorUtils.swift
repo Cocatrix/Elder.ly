254a2f40897ab6b7562aa2f1bc3909aa3f58cf83
Arnaud Ringenbach <arn.ringenbach@gmail.com>
2017-11-30 15:33:58 +0100

    color fix

--- Elder.ly/UIColorUtils.swift
+++ Elder.ly/UIColorUtils.swift
@@ -18,7 +18,7 @@ extension UIColor{
     class func purpleDark() -> UIColor{
         return UIColor(red: 32/255, green: 32/255, blue: 48/255, alpha: 1)
     }
-    class func moisture() -> UIColor{
+    class func greyBlue() -> UIColor{
         return UIColor(red: 65/255, green: 115/255, blue: 115/255, alpha: 1)
     }
     class func orange() -> UIColor{
