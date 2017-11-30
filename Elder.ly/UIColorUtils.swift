63d09e9052c5221d5deed61d7150e70ea946d131
Arnaud Ringenbach <arn.ringenbach@gmail.com>
2017-11-30 15:26:29 +0100

    added custom colors

---
+++ Elder.ly/UIColorUtils.swift
@@ -0,0 +1,27 @@
+//
+//  UIColorUtils.swift
+//  Elder.ly
+//
+//  Created by Arnaud on 30/11/2017.
+//  Copyright © 2017 Old Mojito. All rights reserved.
+//
+
+import Foundation
+
+extension UIColor{
+    class func purpleLight() -> UIColor{
+        return UIColor(red: 146/255, green: 151/255, blue: 191/255, alpha: 1)
+    }
+    class func purple() -> UIColor{
+        return UIColor(red: 57/255, green: 48/255, blue: 74/255, alpha: 1)
+    }
+    class func purpleDark() -> UIColor{
+        return UIColor(red: 32/255, green: 32/255, blue: 48/255, alpha: 1)
+    }
+    class func moisture() -> UIColor{
+        return UIColor(red: 65/255, green: 115/255, blue: 115/255, alpha: 1)
+    }
+    class func orange() -> UIColor{
+        return UIColor(red: 1, green: 138/255, blue: 38/255, alpha: 1)
+    }
+}
