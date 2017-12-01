dcb31404ef73438efcb537477ae798847bb34da9
Arnaud Ringenbach <arn.ringenbach@gmail.com>
2017-12-01 14:46:43 +0100

    delete contact

--- Elder.ly/DetailViewController.swift
+++ Elder.ly/DetailViewController.swift
@@ -133,7 +133,21 @@ class DetailViewController: UIViewController, UIActionSheetDelegate {
     }
     
     func deleteContact() {
+        // dismiss(animated: true, completion: nil)
+        // self.navigationController?.navigationController?.dismiss(animated: true, completion: nil)
         print("Delete pressed")
+        guard let wsId = contact?.wsId else {
+            print("no id")
+            return
+        }
+        WebServicesProvider.sharedInstance.deleteContactOnServer(wsId: wsId, success: {
+            print("success")
+            DispatchQueue.main.async {
+                
+            }
+        }) { (error) in
+            print(error ?? "Error")
+        }
     }
     
     override func didReceiveMemoryWarning() {
