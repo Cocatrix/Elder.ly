ad5aaaab60e3c1f2f472d498e6c5d904ecd4907c
Unknown <drazx974@gmail.com>
2017-12-04 11:35:30 +0100

    Round Buttons in forms

--- Elder.ly/SignUpViewController.swift
+++ Elder.ly/SignUpViewController.swift
@@ -23,12 +23,20 @@ class SignUpViewController: UIViewController, UIPickerViewDelegate, UIPickerView
     
     @IBOutlet weak var requestIndicator: UIActivityIndicatorView!
     @IBOutlet weak var registerButton: UIButton!
+    @IBOutlet weak var loginButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.hideKeyboardWhenTappedAround()
         NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name:NSNotification.Name.UIKeyboardWillShow, object: nil)
         NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name:NSNotification.Name.UIKeyboardWillHide, object: nil) // Do any additional setup after loading the view.
+        
+        // Buttons Styling
+        self.registerButton.layer.cornerRadius = self.registerButton.frame.size.height / 2
+        self.loginButton.layer.borderWidth = 1
+        self.loginButton.layer.borderColor = UIColor.orange().cgColor
+        self.loginButton.layer.cornerRadius = self.loginButton.frame.size.height / 2
+    
         
         let preferencesProfiles = UserDefaults.standard.value(forKey: "elderlyProfiles")
         if ((preferencesProfiles) != nil) {
